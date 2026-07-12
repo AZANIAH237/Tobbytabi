cba82d4d8a71fa79a8071752054d83e736633c14
azaniah237 <etongoalain@gmail.com>
2026-07-12 07:29:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tt-connect-v5';
+const CACHE_NAME = 'tt-connect-v6';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './logo.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
